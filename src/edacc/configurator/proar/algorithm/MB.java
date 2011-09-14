347a35d541184fd9867472d450d1efc37b5e9f4c
Adrian Balint <balint@.(none)>
2011-09-14 15:49:42 +0200

    mutation of the best algorithm added

---
+++ src/edacc/configurator/proar/algorithm/MB.java
@@ -0,0 +1,32 @@
+package edacc.configurator.proar.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import edacc.api.API;
+import edacc.configurator.proar.SolverConfiguration;
+import edacc.configurator.proar.StatisticFunction;
+import edacc.parameterspace.ParameterConfiguration;
+
+public class MB extends PROARMethods {
+
+	public MB(API api, int idExperiment, StatisticFunction statistics, Random rng) {
+		super(api, idExperiment, statistics, rng);
+	}
+
+	@Override
+	public List<SolverConfiguration> generateNewSC(int num, List<SolverConfiguration> lastBestSCs, SolverConfiguration currentBestSC, int level) throws Exception {
+		List<SolverConfiguration> res = new ArrayList<SolverConfiguration>();
+		ParameterConfiguration bestSCP = currentBestSC.getParameterConfiguration();
+		for (int i = 0; i < num; i++) {
+			//ParameterConfiguration paramconfig = api.loadParameterGraphFromDB(idExperiment).getRandomConfiguration(rng);
+			ParameterConfiguration paramconfig = new ParameterConfiguration(bestSCP);
+			api.loadParameterGraphFromDB(idExperiment).mutateParameterConfiguration(rng, paramconfig, rng.nextFloat(), 0.8f);
+			int idSolverConfig = api.createSolverConfig(idExperiment, paramconfig, api.getCanonicalName(idExperiment, paramconfig) + " level " + level);
+			res.add(new SolverConfiguration(idSolverConfig, api.getParameterConfiguration(idExperiment, idSolverConfig), statistics, level));
+		}
+		return res;
+	}
+
+}
