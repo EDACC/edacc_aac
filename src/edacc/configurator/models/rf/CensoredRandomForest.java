08a58829e0fb697721a0a937b54c4187a6149cd1
Daniel D <daniel.diepold@gmail.com>
2012-08-09 14:17:56 +0200

    RF model update, SMBO search method created

--- src/edacc/configurator/models/rf/CensoredRandomForest.java
+++ src/edacc/configurator/models/rf/CensoredRandomForest.java
@@ -7,7 +7,7 @@ import edacc.configurator.models.rf.fastrf.RegtreeBuildParams;
 import edacc.configurator.models.rf.fastrf.RegtreeFit;
 
 public class CensoredRandomForest {
-    RandomForest rf;
+    public RandomForest rf;
     Random rng;
     
     public CensoredRandomForest(int nTrees, int logModel, Random rng) {
@@ -23,8 +23,12 @@ public class CensoredRandomForest {
         params.logModel = logModel;
         params.storeResponses = true;
         params.splitMin = 10;
-                
-                
+        
+        // handle censored values as 10*timeout value
+        for (int i = 0; i < y.length; i++) {
+            if (censored[i]) y[i] = 10.0 * y[i];
+        }
+ 
         for (int i = 0; i < rf.numTrees; i++) {
             int[] sample = new int[y.length];
             for (int j = 0; j < sample.length; j++) sample[j] = rng.nextInt(y.length);
@@ -39,7 +43,10 @@ public class CensoredRandomForest {
             
             rf.Trees[i] = RegtreeFit.fit(theta, instance_features, tree_theta_inst_idxs, tree_y, params);
         }
-        
-
     }
+    
+    public double[][] predict(double[][] theta_inst) {
+        return RandomForest.apply(this.rf, theta_inst);
+    }
+   
 }
